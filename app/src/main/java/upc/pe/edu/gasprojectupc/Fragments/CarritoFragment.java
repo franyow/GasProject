a1db581d64f0bcdda93763ed9349e47962beb239
Franyo <franyow@gmail.com>
2018-05-19 06:20:28 -0500

    modf layout fragments

--- app/src/main/java/upc/pe/edu/gasprojectupc/Fragments/CarritoFragment.java
+++ app/src/main/java/upc/pe/edu/gasprojectupc/Fragments/CarritoFragment.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import upc.pe.edu.gasprojectupc.R;
 
@@ -26,6 +30,7 @@ public class CarritoFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     Button buttonDetallesPago;
+    DatabaseReference mRef;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -70,7 +75,7 @@ public class CarritoFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_carrito, container, false);
 
-        buttonDetallesPago = view.findViewById(R.id.buttonCarr);
+        buttonDetallesPago = view.findViewById(R.id.buttonPagar);
 
         buttonDetallesPago.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +84,9 @@ public class CarritoFragment extends Fragment {
                 getFragmentManager().beginTransaction().replace(R.id.content_main,fragment).commit();
             }
         });
+
+
+
 
 
 
